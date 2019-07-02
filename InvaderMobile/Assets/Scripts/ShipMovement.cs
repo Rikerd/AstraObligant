3ccf4ad7160f36a34aca271af165112fe7da1553
Rikerd <cursedmarkedsasuke@gmail.com>
2019-07-02 16:09:54 -0700

    Ship Movement
    Shipe moves with gyroscope

---
+++ InvaderMobile/Assets/Scripts/ShipMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipMovement : MonoBehaviour
+{
+    public float movementSpeed = 2;
+
+    public float tiltLimit = 0.3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 tilt = Input.acceleration;
+
+        print(tilt);
+
+        if (tilt.x > tiltLimit)
+        {
+            transform.Translate(new Vector3(movementSpeed, 0, 0) * Time.deltaTime);
+        } else if (tilt.x < -tiltLimit)
+        {
+            transform.Translate(new Vector3(-movementSpeed, 0, 0) * Time.deltaTime);
+        }
+    }
+}
