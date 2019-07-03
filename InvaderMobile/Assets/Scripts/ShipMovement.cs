69bbd703bf1b04633f059efb5bcd584f7c6499d8
Rikerd <cursedmarkedsasuke@gmail.com>
2019-07-02 17:08:20 -0700

    Ship Movement Boundaries
    Ships contained in boundaries when moved

--- InvaderMobile/Assets/Scripts/ShipMovement.cs
+++ InvaderMobile/Assets/Scripts/ShipMovement.cs
@@ -8,25 +8,46 @@ public class ShipMovement : MonoBehaviour
 
     public float tiltLimit = 0.3f;
 
+    private Vector2 max;
+    private Vector2 min;
+
+    private Rigidbody2D rb2d;
+
+    private Vector3 movement;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        max = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+        min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+
+        rb2d = GetComponent<Rigidbody2D>();
+
+        movement = new Vector3(movementSpeed, 0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector3 tilt = Input.acceleration;
-
         print(tilt);
-
-        if (tilt.x > tiltLimit)
+        
+        if (moveRightCheck(tilt.x))
         {
             transform.Translate(new Vector3(movementSpeed, 0, 0) * Time.deltaTime);
-        } else if (tilt.x < -tiltLimit)
+        } else if (moveLeftCheck(tilt.x))
         {
             transform.Translate(new Vector3(-movementSpeed, 0, 0) * Time.deltaTime);
         }
+    }
+
+    private bool moveRightCheck(float direction)
+    {
+        return (direction >= tiltLimit && (rb2d.position + new Vector2(movementSpeed, 0) * Time.deltaTime).x <= max.x - 0.28f);
+    }
+
+    private bool moveLeftCheck(float direction)
+    {
+        return (direction <= -tiltLimit && (rb2d.position + new Vector2(-movementSpeed, 0) * Time.deltaTime).x >= min.x + 0.28f);
     }
 }
