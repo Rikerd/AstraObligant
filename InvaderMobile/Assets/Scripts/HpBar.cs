e83e285a682d51e3dd58720004ecf27a974190fa
Rikerd <cursedmarkedsasuke@gmail.com>
2019-07-22 19:32:18 -0700

    Art
    Update ship, bullet art
    Add Health UI (not implement)

---
+++ InvaderMobile/Assets/Scripts/HpBar.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HpBar : MonoBehaviour {
+    private Transform[] hearts;
+    private int heartIndexToDisable;
+
+    // Use this for initialization
+    void Start () {
+        hearts = transform.GetComponentsInChildren<Transform>();
+        heartIndexToDisable = hearts.Length - 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void updateHealth(int dmgTaken)
+    {
+        for (int i = 0; i < dmgTaken; i++)
+        {
+            if (heartIndexToDisable < 0)
+            {
+                return;
+            }
+
+            hearts[heartIndexToDisable].gameObject.SetActive(false);
+            heartIndexToDisable--;
+        }
+    }
+}
