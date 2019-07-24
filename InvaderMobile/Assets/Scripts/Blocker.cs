087c44a6a0791ae3a00e50a759c787c55ec4fab8
Rikerd <cursedmarkedsasuke@gmail.com>
2019-07-24 16:50:10 -0700

    Blocker Movement
    Add "Blocker" horizontal movement and vertical bob

--- InvaderMobile/Assets/Scripts/Blocker.cs
+++ InvaderMobile/Assets/Scripts/Blocker.cs
@@ -4,27 +4,64 @@ using UnityEngine;
 
 public class Blocker : MonoBehaviour
 {
-    public float movementSpeed = 1f;
+    public float horizontalMovementSpeed = 1f;
+    public float verticalMovementSpeed = 0.5f;
+    public float setVerticalMoveTimer = 1f;
 
     public float reflectSpeed = 2f;
+
+    private float verticalMoveTimer;
 
     private Rigidbody2D rb2d;
     private bool movingRight = true;
 
+    private Vector2 verticalMovement;
+    private Vector2 horizontalMovement;
 
 
     // Start is called before the first frame update
-    void Awake()
+    void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
 
+        if (movingRight)
+        {
+            horizontalMovement = new Vector2(horizontalMovementSpeed, 0);
+        }
+        else
+        {
+            horizontalMovement = new Vector2(-horizontalMovementSpeed, 0);
+        }
+
+        verticalMoveTimer = setVerticalMoveTimer;
+
+        verticalMovement = new Vector2(0, verticalMovementSpeed);
+    }
+
+    private void Update()
+    {
+        if (verticalMoveTimer > 0f)
+        {
+            verticalMoveTimer -= Time.deltaTime;
+        }
+        else
+        {
+            verticalMoveTimer = setVerticalMoveTimer;
+            verticalMovement = -verticalMovement;
+        }
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         
+        rb2d.MovePosition(rb2d.position + (horizontalMovement * Time.fixedDeltaTime) + (verticalMovement * Time.fixedDeltaTime));
     }
+
+    //public void OnBecameInvisible()
+    //{
+    //    Destroy(gameObject);
+    //}
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
