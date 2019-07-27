7bd737d6eaa85257120c6646299647fae00f4b13
Rikerd <cursedmarkedsasuke@gmail.com>
2019-07-26 20:00:47 -0700

    Shield (WIP)
    Add recharge timer and shield delay timer
    Add shield values

---
+++ InvaderMobile/Assets/Scripts/Ship Scripts/ShieldController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldController : MonoBehaviour
+{
+    public float setShieldDelay = 0.2f;
+    public float setRechargeDelay = 0.2f;
+
+    private float shieldDelayTimer;
+
+    private bool shielding;
+
+    private ShipHealth hpController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        shielding = false;
+
+        shieldDelayTimer = setShieldDelay;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (shielding)
+        {
+            if (shieldDelayTimer > 0f)
+            {
+                shieldDelayTimer -= Time.deltaTime;
+                return;
+            }
+
+            hpController.ActivateShield();
+        }
+        else
+        {
+            shieldDelayTimer = setShieldDelay;
+
+            hpController.DeactivateShield();
+        }
+    }
+
+    public void StartShield()
+    {
+        shielding = true;
+    }
+
+    public void StopShield()
+    {
+        shielding = false;
+    }
+}
--- InvaderMobile/Assets/Scripts/Ship Scripts/ShipHealth.cs
+++ InvaderMobile/Assets/Scripts/Ship Scripts/ShipHealth.cs
@@ -1,39 +1,94 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShipHealth : MonoBehaviour
 {
     public int maxHP = 3;
 
+    public float maxShield = 100;
+    
+    public float shieldRechargeRate = 2f;
+
     public int currentHP;
+    public float currentShield;
 
     private HpBar hpUIController;
+    private Slider shieldSlider;
 
     private bool shielded;
+
+    private bool recharging;
 
     // Start is called before the first frame update
     void Start()
     {
         currentHP = maxHP;
+        currentShield = maxShield;
 
         hpUIController = GameObject.Find("Health Bar").GetComponent<HpBar>();
+
+        shielded = false;
+        recharging = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        Recharge();
+    }
 
+    private void Recharge()
+    {
+        if (recharging)
+        {
+            if (currentShield < maxShield)
+            {
+                currentShield += shieldRechargeRate * Time.deltaTime;
+            }
+
+            if (currentShield > maxShield)
+            {
+                currentShield = maxShield;
+            }
+        }
     }
 
     public void TakeDamage(int dmg)
     {
-        currentHP -= dmg;
-        hpUIController.updateHealth(dmg);
+        if (!shielded)
+        {
+            currentHP -= dmg;
+            hpUIController.updateHealth(dmg);
+        }
+        else
+        {
+            currentShield -= dmg * 10;
+        }
 
         if (currentHP <= 0)
         {
             Destroy(gameObject);
         }
+    }
+
+    public void ActivateShield()
+    {
+        shielded = true;
+    }
+
+    public void DeactivateShield()
+    {
+        shielded = false;
+    }
+
+    public void ActivateRecharge()
+    {
+        recharging = true;
+    }
+
+    public void DeactivateRecharge()
+    {
+        recharging = false;
     }
 }
